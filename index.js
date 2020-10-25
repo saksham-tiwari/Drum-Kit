b832b3ac6b6cce0e1534035e3628fd8bb2827123
saksham-tiwari <63906176+saksham-tiwari@users.noreply.github.com>
2020-10-25 18:23:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ function makeSound(key){
    var audio= new Audio('sounds/tom-1.mp3');
    audio.play();
    break;
+   case "W":
+   var audio= new Audio('sounds/tom-1.mp3');
+   audio.play();
+   break;
    case "s":
+   var audio= new Audio('sounds/tom-2.mp3');
+   audio.play();
+   break;
+   case "S":
    var audio= new Audio('sounds/tom-2.mp3');
    audio.play();
    break;
@@ -26,7 +34,15 @@ function makeSound(key){
    var audio= new Audio('sounds/tom-3.mp3');
    audio.play();
    break;
+   case "A":
+   var audio= new Audio('sounds/tom-3.mp3');
+   audio.play();
+   break;
    case "d":
+   var audio= new Audio('sounds/tom-4.mp3');
+   audio.play();
+   break;
+   case "D":
    var audio= new Audio('sounds/tom-4.mp3');
    audio.play();
    break;
@@ -34,11 +50,23 @@ function makeSound(key){
    var audio= new Audio('sounds/snare.mp3');
    audio.play();
    break;
+   case "J":
+   var audio= new Audio('sounds/snare.mp3');
+   audio.play();
+   break;
    case "k":
    var audio= new Audio('sounds/crash.mp3');
    audio.play();
    break;
+   case "K":
+   var audio= new Audio('sounds/crash.mp3');
+   audio.play();
+   break;
    case "l":
+   var audio= new Audio('sounds/kick-bass.mp3');
+   audio.play();
+   break;
+   case "L":
    var audio= new Audio('sounds/kick-bass.mp3');
    audio.play();
    break;
